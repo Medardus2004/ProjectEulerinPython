e03cd711b5fa8778dc02de58c09fdc8c28cc2379
Medardus2004 <t.doerfler@medi.de>
2025-03-31 14:29:17 +0200

    feat(python): task066 in dynamic programming

--- task061_080/task066.py
+++ task061_080/task066.py
@@ -2,32 +2,29 @@ from argparse import ArgumentParser
 from tqdm import tqdm
 import gmpy2
 import math
-from fractions import Fraction
+import numpy as np
 
-def bruch(b_0, step , b, D) : 
-    length = step
-    while True : 
-        zaehler = b
-        nenner = 1
-        switcher = 1
 
-        for i in range(1, length ) :
-            switcher = nenner
-            nenner = zaehler
-            zaehler = switcher + nenner
-            print("Step: " , i , " zaehler: " , 2* nenner + zaehler , " nenner: ", nenner)
+memo = {}
 
-        x = (b_0 - 1) * nenner + zaehler
-        y = nenner
-        print("------ x: " , x , " y: ", y , "D", D ,"--------")
-        if x**2 == 1 + D * y**2  :
-            return x
-        else :
-            length += step
-        if length > 12 :
-            break
+def diophantine(D, x, y) :
+    if x**2 - D * y**2 == 1 :
+        return True
+    else :
+        return False
 
-def kettenbruchentwicklung(D):
+def kettenbruch(set_of_b) :
+    length = len(set_of_b)
+    if length in memo:
+        return memo[length]
+    if len(set_of_b) == 1 :
+         result=  np.array([set_of_b[0] , 1])
+    else :
+        result =  np.array([kettenbruch(set_of_b[1:])[1] + set_of_b[0] * kettenbruch(set_of_b[1:])[0], kettenbruch(set_of_b[1:])[0]])   
+    memo[length] = result
+    return result
+
+def create_set(D) :
     set_of_a = []
     set_of_b = [] 
     set_of_x = []
@@ -35,41 +32,45 @@ def kettenbruchentwicklung(D):
     a = math.sqrt(D)
 
     b_0 = math.floor(a)
+    set_of_b.append(b_0)
     x = a - b_0
+    set_of_x.append(str(x)[:6])
+    # set_of_x.append(str(x).split(".")[1][:6])
 
-    set_of_x.append(str(x).split(".")[1][:6])
+    #while True :
+    for _ in range (1, 200) :
+        b = math.floor(1 / x)
+        x = 1 / x - b
 
-    for step in range(1 , 14) :
-        a = 1 / x
-        set_of_a.append(a)
-
-        b = math.floor(a)
+        if str(x)[:6] in set_of_x :    
+            bruch = kettenbruch(set_of_b)
+            global memo 
+            memo = {}
+            # print(set_of_b, bruch)
+            if diophantine(D, bruch[0], bruch[1]) :
+                return bruch[0]
+            
+        set_of_x.append(str(x)[:6])
         set_of_b.append(b)
-
-        x = a - b
-        if str(x).split(".")[1][:6] in set_of_x :
-            print(bruch(b_0, step, set_of_b[-2], D) )
-            break
-        
-        #print("step: ", step ," a = ", a ," b = ", b ," x = ", x)
+    print(D, " is out of range for 200 iterations")
+    return 0
 
 
 def main():
     parser = ArgumentParser(description= "Task 66")
-    parser.add_argument( "maximum_D", help="what is does")
+    parser.add_argument( "maximum_D", help="Diophantine equation")
     args = parser.parse_args()
     largest_D = 0
     largest_x = 1
-    kettenbruchentwicklung(float(args.maximum_D))
-
-    #for d in tqdm(range(2, int(args.maximum_D) + 1)) :
-    #    if gmpy2.is_square(d) :
-    #        continue
-    #    diophan = diophantine(d)
-    #    if largest_x < diophan :
-    #        largest_D = d
-    #        largest_x = diophan
-    #        print(largest_D, largest_x)
+    #create_set(int(args.maximum_D))
+    for d in tqdm(range(2, int(args.maximum_D) + 1)) :
+        if gmpy2.is_square(d) :
+            continue
+        x = create_set(int(d))
+        if largest_x < x  :
+            largest_D = d
+            largest_x = x
+    print(largest_D, largest_x)
 
 if __name__ == "__main__":
     main() 
